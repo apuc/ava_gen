0afe6763b60c4fd13dfb2b18d7187e2a1ba6fc1e
bodya18 <bodya18x@mail.ru>
2021-12-29 11:28:28 +0300

    fix nav routing

--- admin/type_fill/routes.py
+++ admin/type_fill/routes.py
@@ -4,37 +4,37 @@ from services.FillService import FillService
 from services.TypeService import TypeService
 from services.Type_FillService import Type_FillService
 
-type_fill_page = Blueprint('type_fill', __name__, template_folder='templates')
+type_fill_page = Blueprint('TypeFill', __name__, template_folder='templates')
 
 
-@type_fill_page.route('/type_fill', defaults={'current_page': 1})
-@type_fill_page.route('/type_fill/<current_page>')
+@type_fill_page.route('/TypeFill', defaults={'current_page': 1})
+@type_fill_page.route('/TypeFill/<current_page>')
 def index(current_page):
     crud = Crud(Type_Fill)
     crud.pagination(int(request.args.get('page', 1)), 50)
     for i in range(len(crud.items)):
         crud.items[i].type = TypeService.get(crud.items[i].type_id).label
         crud.items[i].fill = FillService.get(crud.items[i].fill_id).value
-    return render_template('admin/type_fill/index.html', crud=crud)
+    return render_template('admin/TypeFill/index.html', crud=crud)
 
 
-@type_fill_page.route('/type_fill/create', methods=['GET'])
+@type_fill_page.route('/TypeFill/create', methods=['GET'])
 def create():
     type_fill_model = Type_Fill()
     types = TypeService.find()
     fills = FillService.find()
-    return render_template('admin/type_fill/form.html', type_fill=type_fill_model, types=types, fills=fills)
+    return render_template('admin/TypeFill/form.html', TypeFill=type_fill_model, types=types, fills=fills)
 
 
-@type_fill_page.route('/type_fill/remove/<id>')
+@type_fill_page.route('/TypeFill/remove/<id>')
 def remove(id):
     Type_FillService.remove(id)
 
     flash('Запись удалена')
-    return redirect(url_for('type_fill.index'))
+    return redirect(url_for('TypeFill.index'))
 
 
-@type_fill_page.route('/type_fill/edit/<id>')
+@type_fill_page.route('/TypeFill/edit/<id>')
 def edit(id):
     type_fill_model = Type_FillService.get(id)
     select_type = TypeService.get(type_fill_model.type_id)
@@ -42,10 +42,10 @@ def edit(id):
     select_fill = FillService.get(type_fill_model.fill_id)
     fills = Fill().query.filter(Type.id != type_fill_model.fill_id).all()
     type_fill_model.fill = FillService.get(type_fill_model.fill_id).value
-    return render_template('admin/type_fill/form.html', type_fill=type_fill_model, types=types, select_type=select_type, fills=fills, select_fill=select_fill)
+    return render_template('admin/TypeFill/form.html', TypeFill=type_fill_model, types=types, select_type=select_type, fills=fills, select_fill=select_fill)
 
 
-@type_fill_page.route('/type_fill/save', methods=['POST'])
+@type_fill_page.route('/TypeFill/save', methods=['POST'])
 def save():
     id = request.form.get('__id')
     params = dict(
@@ -54,4 +54,4 @@ def save():
         )
     Type_FillService.save(id, **params)
     flash('Данные сохранены')
-    return redirect(url_for('type_fill.index'))
+    return redirect(url_for('TypeFill.index'))
